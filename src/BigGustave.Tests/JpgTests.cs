e42bfd9a8202a4904c95a259eab65c34d37e0927
Eliot Jones <elioty@hotmail.co.uk>
2021-05-08 15:09:06 -0400

    swap commented test to test of gritty with chroma subsampling

--- src/BigGustave.Tests/JpgTests.cs
+++ src/BigGustave.Tests/JpgTests.cs
@@ -18,13 +18,15 @@
                 green, blackish
             };
             
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images", "jpg", "2by2checkerboard-green.jpg");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images", "jpg", "8by8.jpg");
 
-            using (var stream = File.OpenRead(path))//@"C:\git\python\micro-jpeg-visualizer\images\porsche.jpg"))
+            using (var stream = File.OpenRead(path))// @"C:\git\python\micro-jpeg-visualizer\images\gritty.jpg"))
             {
                 var img = Jpg.Open(stream);
 
                 var i = 0;
+
+                var png = PngBuilder.Create(img.Width, img.Height, false);
 
                 for (var row = 0; row < img.Height; row++)
                 {
@@ -32,11 +34,15 @@
                     {
                         var pixel = img.GetPixel(col, row);
 
+                        png.SetPixel(pixel, col, row);
+
                         Assert.Equal(expected[i], pixel);
 
                         i++;
                     }
                 }
+
+                // File.WriteAllBytes(@"C:\temp\gritty.jpg", png.Save());
 
                 Assert.Equal(2, img.Width);
                 Assert.Equal(2, img.Height);
